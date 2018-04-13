1c143f53d046fc5abf8ffbf74ddb6711eeddf1d7
derrick lee <dlee@linux60822.dc.engr.scu.edu>
2018-04-13 15:35:42 -0700

    Add part 1

---
+++ lab1/main.c
@@ -0,0 +1,31 @@
+#include<unistd.h>
+#include<sys/wait.h>
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char *argv[]) {
+  char *input = NULL;
+  size_t size;
+
+  while (1) {
+    getline(&input, &size, stdin);
+    input[strlen(input) - 1] = 0; // remove the newline at end?
+
+    // exit on "exit"
+    if (strcmp(input, "exit") == 0) {
+      return 0;
+    }
+
+    if (fork() == 0) {
+      // child code
+      execve(input, argv, 0);
+      break; // exits invalid program
+    } else {
+      // parent
+      int status;
+      waitpid(-1, &status, 0);
+    }
+  }
+
+  return 0;
+}
