4faaa6385f72cca6da61f33a0f23b33ab7c77f63
drklee3 <dlee3@scu.edu>
2018-05-04 17:01:29 -0700

    Update bash script with functions

--- lab3/run.sh
+++ lab3/run.sh
@@ -1,18 +1,31 @@
-#1/bin/bash
-echo "Generating random files in /local/weka/"
-head -c 1G < /dev/urandom > /local/weka/1G.bin
-echo "1G file generated"
-head -c 2500M < /dev/urandom > /local/weka/2.5G.bin
-echo "2.5G file generated"
-head -c 5G < /dev/urandom > /local/weka/5G.bin
-echo "5G file generated"
+#!/bin/bash
+files_list=( "1G.bin" "2.5G.bin" "5G.bin" )
+
+gen_files() {
+  echo "Generating random files in /local/weka/"
+  head -c 1G < /dev/urandom > /local/weka/1G.bin
+  echo "1G file generated"
+  head -c 2500M < /dev/urandom > /local/weka/2.5G.bin
+  echo "2.5G file generated"
+  head -c 5G < /dev/urandom > /local/weka/5G.bin
+  echo "5G file generated"
+}
+
+
+run_test() {
+  echo "Running sequential tests"
+  for fname in "${files_list[@]}"
+  do
+    echo "Reading $fname"
+    time ./seqread.o /local/weka/$fname
+  done
+}
+
+
+if [ $1 != "skipgen" ]; then
+  gen_files
+fi
 
 echo "Compiling c files"
 make
-
-echo "Running sequential tests"
-for i in {1..3}
-do
-  echo "Trial $i"
-  ./seqread.o /local/weka/1G.bin
-done
+run_test
