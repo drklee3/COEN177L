c3017d5485fbd49b2d7b3071647106ea1724ea6c
drklee3 <dlee3@scu.edu>
2018-05-05 00:14:17 -0700

    Add more functionality to run.sh

--- lab3/run.sh
+++ lab3/run.sh
@@ -1,31 +1,123 @@
 #!/bin/bash
-files_list=( "1G.bin" "2.5G.bin" "5G.bin" )
+run_seq="true"
+run_rand="true"
+skip_gen="false"
+trials="3"
+log_file="output.log"
+file_path="/local/weka/"
+file_suffix="bin"
+# file sizes for head
+file_sizes=( "1G" "2500M" "5G" )
+# file sizes in bytes (ish)
+file_sizes_bytes=( 1073000000 2620000000 5368000000 )
+# pretty file sizes
+file_list=( "1G" "2.5G" "5G" )
 
 gen_files() {
-  echo "Generating random files in /local/weka/"
-  head -c 1G < /dev/urandom > /local/weka/1G.bin
-  echo "1G file generated"
-  head -c 2500M < /dev/urandom > /local/weka/2.5G.bin
-  echo "2.5G file generated"
-  head -c 5G < /dev/urandom > /local/weka/5G.bin
-  echo "5G file generated"
+  echo "Generating random files in $file_path"
+  for index in ${!file_sizes[*]};
+  do
+    head -c ${file_sizes[$index]} < /dev/urandom > ${file_path}${file_list[$index]}.${file_suffix}
+    echo "${file_list[$index]} file generated"
+  done
 }
 
-
-run_test() {
-  echo "Running sequential tests"
-  for fname in "${files_list[@]}"
+check_files() {
+  echo "Checking random files..."
+  for index in ${!file_sizes[*]};
   do
-    echo "Reading $fname"
-    time ./seqread.o /local/weka/$fname
+    # build filepath
+    full_file_path="${file_path}${file_list[$index]}.${file_suffix}"
+    # check if readable
+    if [[ -e $full_file_path && -r $full_file_path ]]
+    then
+      actual_size=$(stat -c%s $full_file_path)
+      # check if file size large enough in case head failed or something
+      if [ $actual_size -le ${file_sizes_bytes[index]} ]; then
+        echo "Error: $full_file_path is not large enough.  ($actual_size bytes)"
+        exit 2
+      fi
+    else
+      echo "Cannot read $full_file_path, it doesn't exist or you don't own it."
+      exit 2
+    fi
   done
 }
 
 
-if [ $1 != "skipgen" ]; then
+run_test() {
+  for fname in "${file_list[@]}"
+  do
+    (printf "Reading $fname file\n" |& tee -a $log_file)
+    (time ./$1.o "${file_path}${fname}.${file_suffix}") |& tee -a $log_file
+    (printf "%s\n\n" "-----------------------" |& tee -a $log_file)
+  done
+}
+
+run_trial() {
+  for i in $(seq 1 $trials); do
+    if [ "$1" == "seqread" ]; then
+      echo "Starting Trial $i (sequential)" |& tee -a $log_file
+    else
+      echo "Starting Trial $i (random)" |& tee -a $log_file
+    fi
+    run_test $1
+    echo "Finished Trial $i" |& tee -a $log_file
+    (printf "%s\n\n" "=======================" |& tee -a $log_file)
+  done
+}
+
+usage() {
+  echo "Usage: $0 [-rsSh] [-l file] [-t trials] [-p path] [-s suffix]"
+  echo "  -r          only run random reads (default runs both)"
+  echo "  -s          only run sequential reads (default runs both)"
+  echo "  -S          skip generation of random files"
+  echo "  -h          display help"
+  echo "  -l file     specify log file for output (default output.log)"
+  echo "  -t trials   specify number of trials to run each (default 3)"
+  echo "  -p path     specify path for random data files (default /local/weka/)"
+  echo "  -o suffx    specify random data file suffix (default .bin)"
+  exit 1
+}
+
+
+while getopts "rsSht:p:" flag; do
+  case $flag in
+    r) run_seq="false" ;;
+    s) run_rand="false" ;;
+    S) skip_gen="true" ;;
+    h) usage ;;
+    t) trials=${OPTARG} ;;
+    p) file_path="${OPTARG}" ;;
+    *) echo "Unexpected option ${flag}"; exit 2;;
+  esac
+done
+
+
+# run stuff
+if [ "$skip_gen" == "false" ]; then
   gen_files
+else
+  echo "Skipping generation of random files."
 fi
 
+# check if files exist and are correct size
+check_files
+
+# compile files, if that wasn't obvious enough
 echo "Compiling c files"
 make
-run_test
+
+# run tests for # trials
+echo "Running $trials trials"
+if [ $run_seq == "true" ]; then
+  run_trial seqread
+else
+  echo "Skipping sequential reads."
+fi
+
+if [ $run_rand == "true" ]; then
+  run_trial randread
+else
+  echo "Skipping random reads."
+fi
