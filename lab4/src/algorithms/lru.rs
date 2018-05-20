19720670c86e0c96d17853c2498ff3f577a58712
drklee3 <dlee3@scu.edu>
2018-05-20 16:27:54 -0700

    Optimize lru to not shift elements

--- lab4/src/algorithms/lru.rs
+++ lab4/src/algorithms/lru.rs
@@ -4,6 +4,8 @@ pub struct Lru {
   table: Vec<u64>,
   /// Size of page table
   size: usize,
+  /// Index position
+  index: usize,
 }
 
 impl Lru {
@@ -11,21 +13,32 @@ impl Lru {
     Lru {
       table: Vec::with_capacity(size),
       size: size,
+      index: 0,
     }
   }
 
   /// Handles a page request, returns true if page fault occurred
   pub fn handle_page_request(&mut self, page_request: u64) -> bool {
     if !self.table.contains(&page_request) {
-      println!("Page fault: {}", page_request);
+      println!("Page {} caused a page fault", page_request);
 
-      // remove first page if over capacity
-      if self.table.len() >= self.size {
-        self.table.remove(0);
+      // we can't really use vec[i] past the number of elements even though
+      // the _capacity_ is large enough. 
+
+      // under capacity, just push to end
+      if self.table.len() < self.size {
+        self.table.push(page_request);
+      } else {
+        // not under capacity
+        if let Some(elem) = self.table.get_mut(self.index) {
+          trace!("Modified {} -> {} at index {}", *elem, page_request, self.index);
+          *elem = page_request;
+        }
       }
 
-      // push new page request
-      self.table.push(page_request);
+      // incrememnt circular "loop"
+      self.index = (self.index + 1) % self.size;
+
       debug!("{:?}", self.table);
       return true;
     }
