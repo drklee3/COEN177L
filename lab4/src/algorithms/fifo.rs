b9d290713f7ab5457826a2412fb5e1b724604754
drklee3 <dlee3@scu.edu>
2018-05-20 16:49:55 -0700

    Optimize fifo algorithm

--- lab4/src/algorithms/fifo.rs
+++ lab4/src/algorithms/fifo.rs
@@ -4,28 +4,33 @@ pub struct Fifo {
   table: Vec<u64>,
   /// Size of page table
   size: usize,
+  /// Index position
+  index: usize,
 }
 
 impl Fifo {
   pub fn new(size: usize) -> Self {
     Fifo {
-      table: Vec::with_capacity(size),
+      table: vec![0; size],
       size: size,
+      index: 0,
     }
   }
 
   /// Handles a page request, returns true if page fault occurred
   pub fn handle_page_request(&mut self, page_request: u64) -> bool {
     if !self.table.contains(&page_request) {
-      println!("Page fault: {}", page_request);
+      println!("Page {} caused a page fault", page_request);
 
-      // remove first page if over capacity
-      if self.table.len() >= self.size {
-        self.table.remove(0);
+      if let Some(elem) = self.table.get_mut(self.index) {
+        trace!("Modified {} -> {} at index {}", *elem, page_request, self.index);
+        *elem = page_request;
+      } else {
+        unreachable!();
       }
 
-      // push new page request
-      self.table.push(page_request);
+      self.index = (self.index + 1) % self.size;
+
       debug!("{:?}", self.table);
       return true;
     }
