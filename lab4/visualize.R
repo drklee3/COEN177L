aba42be7637f430f7e9b546641580bfbe431823b
drklee3 <dlee3@scu.edu>
2018-05-22 14:22:02 -0700

    Use separate csv files for visualizing data

---
+++ lab4/visualize.R
@@ -0,0 +1,34 @@
+library(ggplot2)
+library(reshape2)
+theme_set(theme_bw())
+getwd()
+
+multiMerge = function() {
+  filenames = list.files(path = "./", pattern="*.csv", full.names = TRUE)
+  datalist = lapply(filenames, function(x) {
+    read.csv(file = x, header = TRUE, stringsAsFactors = FALSE)
+  })
+  Reduce(function(x,y) {
+    merge(x, y, all = TRUE, id="table_size")
+  }, datalist)
+}
+
+data <- multiMerge()
+head(data)
+
+chart_data <- melt(data, id="table_size")
+head(chart_data)
+names(chart_data) <- c("size", "Algorithm", "value")
+
+# plot 
+ggplot() +
+  geom_line(data = chart_data,
+    aes(x = size, y = value, color = Algorithm),
+    size = 1) +
+  labs(title="Hit Rate of Page Replacement Algorithms", 
+    y = "Hit Rate %", 
+    x = "Table size",
+    color = NULL)
+
+ggsave("plot.png", device = png())
+dev.off()
