00fd398d2bddc07c01e76d81906b819bf0a94a02
drklee3 <dlee3@scu.edu>
2018-05-22 19:22:10 -0700

    Switch to clock algorithm for second chance

--- lab4/src/algorithms/second_chance.rs
+++ lab4/src/algorithms/second_chance.rs
@@ -9,6 +9,15 @@ pub struct SecondChancePage {
   referenced: bool,
 }
 
+impl SecondChancePage {
+  fn new() -> Self {
+    SecondChancePage {
+      number: 0,
+      referenced: false,
+    }
+  }
+}
+
 impl fmt::Debug for SecondChancePage {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     if self.referenced {
@@ -28,71 +37,64 @@ pub struct SecondChance {
   table: Vec<SecondChancePage>,
   /// Size of page table
   size: usize,
+  /// Index position
+  index: usize,
 }
 
 impl SecondChance {
   pub fn new(size: usize) -> Self {
     SecondChance {
-      table: Vec::with_capacity(size),
+      table: vec![SecondChancePage::new(); size],
       size: size,
+      index: 0,      
     }
   }
 
   /// Handles a page request, returns true if page fault occurred
   pub fn handle_page_request(&mut self, page_request: u64) -> bool {
-    // search if contains, use any() to search with struct field
-    if !self.table.iter().any(|x| x.number == page_request) {
+    // get page index, returns None if doesn't exist in table
+    let page_index = self.table
+      .iter()
+      .position(|x| x.number == page_request);
+    
+    // doesn't contain page number, not in memory
+    if page_index.is_none() {
       println!("Page {} caused a page fault", page_request);
 
-      // remove first page if over capacity
-      if self.table.len() >= self.size {
-        // cannot use iterator over self.table here
-        loop {
-          // safe to unwrap, len has to be at least 1
-          let mut page = self.table
-            .first()
-            .unwrap()
-            .clone();
-          
-          // not referenced, can throw out
-          if !page.referenced {
-            // safe to unwrap, this only runs when table @ max capacity
-            trace!("Removed entry: {:?}", self.table.first().unwrap());
-            self.table.remove(0);
-            // removed a page, can exit loop
-            break;
-          }
-
-          trace!("Reset entry: {:?}", self.table.first().unwrap());
-          // page is referenced, reset referenced and move to end of list
-          self.table.remove(0);
-          page.referenced = false;
-          self.table.push(page);
+      loop {
+        // safe to unwrap, vec is initialized to be full
+        let page = self.table
+          .get_mut(self.index)
+          .unwrap();
+        
+        // move to next page, circular loop
+        self.index = (self.index + 1) % self.size;
+        
+        if !page.referenced {
+          // replace page with new page request #, referenced should still be false
+          trace!("Replace entry: {} -> {}", page.number, page_request);
+          page.number = page_request;
+          break;
         }
+
+        trace!("Reset entry: {}", page.number);
+        page.referenced = false;
       }
-    
 
-      // push new page request
-      let new_page = SecondChancePage {
-        number: page_request,
-        referenced: false,
-      };
-
-      self.table.push(new_page);
       debug!("{:?}", self.table);
       return true;
     }
+    // self.index = (self.index + 1) % self.size;
 
-    // get page index
-    let index = self.table
-      .iter()
-      .position(|x| x.number == page_request)
-      .unwrap();
+    // safe to unwrap here, returns before if None
+    let index = page_index.unwrap();
 
     // update page to be referenced
     if let Some(page) = self.table.get_mut(index) {
       trace!("Update entry: {:?}", page);
       page.referenced = true;
+    } else {
+      error!("Failed to get table value @ {}", index);
     }
 
     debug!("{:?}", self.table);
