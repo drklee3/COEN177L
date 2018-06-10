b357a5cf8bbcd1a5cea6cbdcbf3438fe14d38216
drklee3 <dlee3@scu.edu>
2018-06-10 14:13:56 -0700

    Make bench generation more configurable

--- lab5-final/gen_benches.sh
+++ lab5-final/gen_benches.sh
@@ -2,10 +2,16 @@
 # this script generates a series of rust macros for benchmarks
 # of threads 1 to 8 each with a range of 0 to 100% lock holds
 
-for threads in $(seq 1 8); do
-  for percent in $(seq 0.0 .01 1.0); do
+THREADS_MIN="1"
+THREADS_MAX="8"
+PERCENT_MIN="0.0"
+PERCENT_MAX="1.0"
+PERCENT_STEP=".01"
+
+for threads in $(seq $THREADS_MIN $THREADS_MAX); do
+  for percent in $(seq $PERCENT_MIN $PERCENT_STEP $PERCENT_MAX); do
     percent_name="${percent/./}"
-    printf "benchtest!{mutex_%dt_%sp, %d, %s}\n" $threads $percent_name $threads $percent
+    echo "benchtest!{mutex_${threads}t_${percent_name}p, ${threads}, ${percent}}"
   done
   echo ""
 done
